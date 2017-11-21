d0737d63693c60e66168b231e1e2b877e86b406b
Nabawy90 <33811382+Nabawy90@users.noreply.github.com>
2017-11-21 22:39:42 +0000

    added relative path

--- ExpediaPriceLocator/PriceSearch/PriceSearch_Test.py
+++ ExpediaPriceLocator/PriceSearch/PriceSearch_Test.py
@@ -1,6 +1,7 @@
 import sys
 import os
-sys.path.append('..')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+#sys.path.append('..')
 from GUIAutomation import ExpediaSearch
 from GUIAutomation import XPATH_STD
 from Validation import Validator
